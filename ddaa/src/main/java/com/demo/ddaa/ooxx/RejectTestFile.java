6f6c0d33fd773fcfeda6e05d140d9bb6a02c2627
williamsGitHub <450401627@qq.com>
2020-04-17 21:04:19 +0800

    Update RejectTestFile.java

--- ddaa/src/main/java/com/demo/ddaa/ooxx/RejectTestFile.java
+++ ddaa/src/main/java/com/demo/ddaa/ooxx/RejectTestFile.java
@@ -3,5 +3,6 @@ package com.demo.ddaa.ooxx;
 public class RejectTestFile {
     public static void main(String[] args) {
         System.out.println("this is fist write");
+        System.out.println("我GITHUB上别人新增的代码");
     }
 }
