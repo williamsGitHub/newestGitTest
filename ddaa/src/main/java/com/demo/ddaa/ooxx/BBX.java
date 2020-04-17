2b56c066b992cd580822382f769b75dfd70017fc
williamsGitHub <450401627@qq.com>
2020-04-17 20:59:20 +0800

    Update BBX.java

--- ddaa/src/main/java/com/demo/ddaa/ooxx/BBX.java
+++ ddaa/src/main/java/com/demo/ddaa/ooxx/BBX.java
@@ -3,5 +3,6 @@ package com.demo.ddaa.ooxx;
 public class BBX {
     public static void main(String[] args) {
         System.out.println("bbssssssww");
+        System.out.println("GitHub写入的数据");
     }
 }
