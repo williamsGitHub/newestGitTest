24d01e87bf83e15e6b735dced5a108c1d96dbfd8
williamsGitHub <450401627@qq.com>
2020-04-17 17:08:44 +0800

    the second commit1

--- ddaa/src/main/java/com/demo/ddaa/ooxx/MyGitLearn.java
+++ ddaa/src/main/java/com/demo/ddaa/ooxx/MyGitLearn.java
@@ -3,5 +3,6 @@ package com.demo.ddaa.ooxx;
 public class MyGitLearn {
     public static void main(String[] args) {
         System.out.println("this is first coding");
+        System.out.println("this is second coding");
     }
 }
