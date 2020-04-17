c47862f04a69f67b274356efbb463a66537d5c31
williamsGitHub <450401627@qq.com>
2020-04-17 20:59:48 +0800

    the second commit123222

--- ddaa/src/main/java/com/demo/ddaa/ooxx/BBX.java
+++ ddaa/src/main/java/com/demo/ddaa/ooxx/BBX.java
@@ -3,5 +3,6 @@ package com.demo.ddaa.ooxx;
 public class BBX {
     public static void main(String[] args) {
         System.out.println("bbssssssww");
+        System.out.println("IDEA写入的数据");
     }
 }
