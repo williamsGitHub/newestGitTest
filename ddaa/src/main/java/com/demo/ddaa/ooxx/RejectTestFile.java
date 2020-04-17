e6e73c4f15477d7ca79e549c50940ee8c74a77a4
williamsGitHub <450401627@qq.com>
2020-04-17 21:05:00 +0800

    将数据先提交到本次仓库

--- ddaa/src/main/java/com/demo/ddaa/ooxx/RejectTestFile.java
+++ ddaa/src/main/java/com/demo/ddaa/ooxx/RejectTestFile.java
@@ -3,5 +3,6 @@ package com.demo.ddaa.ooxx;
 public class RejectTestFile {
     public static void main(String[] args) {
         System.out.println("this is fist write");
+        System.out.println("我IDEA想改动的代码");
     }
 }
