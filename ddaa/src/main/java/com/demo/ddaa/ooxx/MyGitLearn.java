a103f3d7e210529a32edcbba4ae21ad0f0b1851b
williamsGitHub <450401627@qq.com>
2020-04-17 17:15:22 +0800

    Update MyGitLearn.java

--- ddaa/src/main/java/com/demo/ddaa/ooxx/MyGitLearn.java
+++ ddaa/src/main/java/com/demo/ddaa/ooxx/MyGitLearn.java
@@ -4,5 +4,6 @@ public class MyGitLearn {
     public static void main(String[] args) {
         System.out.println("this is first coding");
         System.out.println("this is second coding");
+        System.out.println("this is otherCoder ChangeData coding");
     }
 }
